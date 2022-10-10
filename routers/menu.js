1bf5c66bed4561f3891c8ffce47cdaf905449f51
Strevino2 <strevino2@hotmail.com>
2022-10-09 19:33:26 -0500

    deleted one line

--- routers/menu.js
+++ routers/menu.js
@@ -3,7 +3,6 @@ const menuController = require("../controllers/menu");
 const router = express.Router();
 const { authenticateToken } = require("../controllers/auth");
 
-// router.get('/menu', authenticateToken, menuController.getAllMenu)
 router.get("/menu", menuController.getAllMenu);
 router.get("/menu/:id", menuController.getmenuByID);
 router.post('/menu', authenticateToken, menuController.createMenu)
